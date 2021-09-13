e1b200503a40e2c2d46b51b0650094a7b8576e37
caseylbell <89037371+caseylbell@users.noreply.github.com>
2021-09-13 14:48:06 -0700

    Delete worksample directory

--- worksample/6DdWgldG6aa_captions.js
+++
@@ -1,1 +0,0 @@
-window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.51.24979.0%0D%0A%0D%0A00:00:00.032%20--%3E%2000:00:03.136%0D%0ALet%E2%80%99s%20practice.%20Select%20two%20accessibility%20laws%20for%0D%0A%0D%0A00:00:03.168%20--%3E%2000:00:04.518%0D%0Ae-learning%20settings.%0D%0A%0D%0A"}')
