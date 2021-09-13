3722c672b03a4f8b39f8151278d188d9298f7476
caseylbell <89037371+caseylbell@users.noreply.github.com>
2021-09-13 14:51:39 -0700

    Add files via upload

---
+++ worksample/story_content/5ujMt3lgwpY_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.51.24979.0%0D%0A%0D%0A00:00:00.288%20--%3E%2000:00:03.146%0D%0AWhat%20does%20WCAG%20stand%20for?%0D%0A%0D%0A"}')
