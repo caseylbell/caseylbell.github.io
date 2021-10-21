f77899ca028542c1c8684e107d445cdd84e4dc02
caseylbell <89037371+caseylbell@users.noreply.github.com>
2021-10-20 17:20:16 -0700

    Final version

--- ist541/finalproject/js/gifs.js
+++ ist541/finalproject/js/gifs.js
@@ -4,7 +4,7 @@ $( function() {
 
       if ($(this).attr("id") == "fivesecs") {
          $("#feedback").css("color","green");
-         $("#feedback").html("Yes, this is where you first click on your Animate Timeline to then be able to right click in the same spot and insert a keyframe!");
+         $("#feedback").html("Yes, this is where you right click and insert a keyframe!");
       }
 
   $("#feedback").css("backgroundColor","yellow");
