6b79d1e52b362d4be5db2e4c67e52c7c825dfb77
caseylbell <89037371+caseylbell@users.noreply.github.com>
2021-11-12 01:02:53 -0800

    Add files via upload

---
+++ worksample/story_content/5Yg6ro8ecID_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.52.25247.0%0D%0A%0D%0A00:00:00.032%20--%3E%2000:00:05.075%0D%0ALet%E2%80%99s%20practice.%20List%20the%20four%20categories%20of%20mental%20and%20physical%20involvement.%0D%0A%0D%0A"}')
---
+++ worksample/story_content/5zRH6HO4dhH_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.52.25247.0%0D%0A%0D%0A00:00:00.032%20--%3E%2000:00:03.792%0D%0ASelect%20current%20standards%20for%20accessible%20e-learning%20design.%0D%0A%0D%0A"}')
